904e94540be0bba58c2b21d3dd03c94e3c296952
Emmanuel Merida <Emmanuel-Merida@outlook.com>
2025-11-26 00:00:54 -0600

    Agregando funcionalidad para usuarios

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './app/prestamil/core/interceptors/auth.interceptor';
+import { AuthErrorInterceptor } from './app/prestamil/core/interceptors/auth-error.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -16,6 +19,8 @@ bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, AppRoutingModule), 
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthErrorInterceptor, multi: true }
   ]
 }).catch((err) => console.error(err));
